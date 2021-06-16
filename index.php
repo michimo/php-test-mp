434100f92d64ddc57e93455bfebd6aacf8779f58
Oskars <oskars.neilands@gmail.com>
2021-06-16 19:31:48 +0300

    directory structure changed

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-    require_once 'helpers.php';
-    require_once 'functions.php';
+    require_once 'incl/helpers.php';
+    require_once 'incl/functions.php';
 
     session_start();
 
@@ -147,74 +147,7 @@
         <div class="results"></div>
 
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="js/script.js"></script>
 
-        <script type="text/javascript">
-
-            $(document).ready(function () {
-
-                $("button").click(function(e) {
-                    e.preventDefault();
-
-                    $(".results").fadeOut(0);
-
-                    $.ajax({
-                        type: "POST",
-                        url: "index.php",
-                        data: {
-                            "get_holidays": true,
-                            "country": $("select").val(),
-                            "year": $("input").val()
-                        },
-                        success: function (resp) {
-
-                            var data = JSON.parse(resp);
-
-                            console.log(data);
-
-                            if ( data == "small" ) {
-                                alert("Too small number for year!");
-                            }
-
-                            if ( data == "large" ) {
-                                alert("Too large number for year!");
-                            }
-
-                            $(".results").empty();
-
-                            $(".results").append("<hr/><p>Holiday count: " + data.public_holidays_count + "</p>");
-
-                            var today = data.is_today_holiday ? 'Holiday' : (data.is_today_workday ? 'Workday' : data.weekday_of_today)
-                            $(".results").append("<p>Today: " + today + "</p>");
-
-                            $(".results").append("<p>Maximum number of free days (incl. holidays + weekend) in a row: " + data.free_days_in_row_count + "</p><hr/>");
-
-                            if ( data.public_holidays.length == 0 ) {
-
-                                $(".results").append("There are no public holidays for this country!");
-
-                            } else {
-
-                                $(".results").append("<ul></ul>");
-
-                                var holidayItem = "";
-                                $.each(data.public_holidays, function (key, holiday) {
-                                    var days = "";
-                                    $.each(holiday, function (subkey, holidayName) {
-                                        days += "<li>" + holidayName + "</li>";
-                                    });
-                                    holidayItem += "<li>" + key + "<ul>" + days + "</ul></li>";
-                                });
-                                $(".results > ul").append(holidayItem);
-                            }
-
-                            $(".results").fadeIn(400);
-
-                        },
-                    });
-
-                });
-            });
-
-        </script>
     </body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+
+    $("button").click(function(e) {
+        e.preventDefault();
+
+        $(".results").fadeOut(0);
+
+        $.ajax({
+            type: "POST",
+            url: "index.php",
+            data: {
+                "get_holidays": true,
+                "country": $("select").val(),
+                "year": $("input").val()
+            },
+            success: function (resp) {
+
+                var data = JSON.parse(resp);
+
+                console.log(data);
+
+                if ( data == "small" ) {
+                    alert("Too small number for year!");
+                }
+
+                if ( data == "large" ) {
+                    alert("Too large number for year!");
+                }
+
+                $(".results").empty();
+
+                $(".results").append("<hr/><p>Holiday count: " + data.public_holidays_count + "</p>");
+
+                var today = data.is_today_holiday ? 'Holiday' : (data.is_today_workday ? 'Workday' : data.weekday_of_today)
+                $(".results").append("<p>Today: " + today + "</p>");
+
+                $(".results").append("<p>Maximum number of free days (incl. holidays + weekend) in a row: " + data.free_days_in_row_count + "</p><hr/>");
+
+                if ( data.public_holidays.length == 0 ) {
+
+                    $(".results").append("There are no public holidays for this country!");
+
+                } else {
+
+                    $(".results").append("<ul></ul>");
+
+                    var holidayItem = "";
+                    $.each(data.public_holidays, function (key, holiday) {
+                        var days = "";
+                        $.each(holiday, function (subkey, holidayName) {
+                            days += "<li>" + holidayName + "</li>";
+                        });
+                        holidayItem += "<li>" + key + "<ul>" + days + "</ul></li>";
+                    });
+                    $(".results > ul").append(holidayItem);
+                }
+
+                $(".results").fadeIn(400);
+
+            },
+        });
+
+    });
+});
